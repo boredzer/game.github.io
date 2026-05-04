1912396c3849886053f4baf46645b40f368d7dcd
Pakorn-P <boredser@gmail.com>
2026-05-05 03:23:31 +0700

    ดเ้ดเ

--- docs/Build/docs.loader.js
+++ docs/Build/docs.loader.js
@@ -52,7 +52,7 @@ function createUnityInstance(canvas, config, onProgress) {
       preserveDrawingBuffer: false,
       powerPreference: 2,
     },
-    wasmFileSize: 77050658,
+    wasmFileSize: 77051579,
     streamingAssetsUrl: "StreamingAssets",
     downloadProgress: {},
     deinitializers: [],
